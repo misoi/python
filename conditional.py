6a244c4eba473bb763cd1cf68361e88518087b12
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-03 18:40:27 +0300

    added more if statements

--- conditional.py
+++ conditional.py
@@ -6,3 +6,14 @@ if answer == "Left":
 
 # Will the above print statement print to the console?
 # Set response to 'Y' if you think so, and 'N' if you think not.
+
+def using_control_once():
+    if 100==10**2:
+        return "Success #1"
+
+def using_control_again():
+    if 7<10:
+        return "Success #2"
+
+print using_control_once()
+print using_control_again()
