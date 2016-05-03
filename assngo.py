762647697b52f9455846a4cfc6eb966fb16b542b
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-03 15:54:16 +0300

    assignment

---
+++ assngo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+moringa={'name':'moringa school','location':'ngong road','age':'old'}
+def replace(moringa):
+    for i in moringa:
+         if i=='name':
+
+            replace('name','school')
+print moringa
