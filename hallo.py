7e2002f931387ff23e6087a5edbb9f3bcab28832
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-03 15:30:52 +0300

    added more print messages

--- hallo.py
+++ hallo.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python
 print 'hallo world'
+print 'hallo world again'
+print 'i like typing this'
+print 'this is fun'
---
+++ python.py
@@ -0,0 +1,15 @@
+!#/usr/bin/python
+moringa{
+  'name':'moringa school',
+  'location':'ngong road',
+  'age':'old'
+}
+
+ def replace('key','value'):
+ for i in moringa:
+ if i==key:
+ value[i]=value
+ replace('name',school)
+
+ print moringa
+
