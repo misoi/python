e3002ac54424dcb5e6b6fca5d9fc3706d3fa8ffb
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-03 18:25:23 +0300

    learning booleans

---
+++ boolean.py
@@ -0,0 +1,4 @@
+my_int=7
+my_float=1.23
+my_bool=True
+# Set the variables to the values listed in the instructions!
---
+++ fib.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+def fib2(n):
+    result=[]
+    a,b=0,1
+    while b<10:
+        result.append(b)
+        a,b=b,a+b
+    return results
