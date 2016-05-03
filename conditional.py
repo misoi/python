03d68f8a4703b23b0fac868e19eb2291d93f2c99
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-03 18:40:56 +0300

    added else if

--- conditional.py
+++ conditional.py
@@ -17,3 +17,17 @@ def using_control_again():
 
 print using_control_once()
 print using_control_again()
+
+answer = "'Tis but a scratch!"
+
+def black_knight():
+    if answer == "'Tis but a scratch!":
+        return True
+    else:
+        return  False      # Make sure this returns False
+
+def french_soldier():
+    if answer == "Go away, or I shall taunt you a second time!":
+        return True
+    else:
+        return False       # Make sure this returns False
