4ab77b32259936cd2e509a9a0d0079a6b5c646d9
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-04 23:56:27 +0300

    added a function that takes no argument

--- codeacademy/nemvarfun.py
+++ codeacademy/nemvarfun.py
@@ -8,3 +8,6 @@ print "arg1: %r, arg2: %r" % (arg1, arg2)
 # this just takes one argument
 def print_one(arg1):
 print "arg1: %r" % arg1
+# this one takes no arguments
+def print_none():
+print "I got nothin'."
