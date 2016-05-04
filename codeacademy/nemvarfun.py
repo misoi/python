586ec4bc3a229f8a8decd49d9cf7cd82a7e59c98
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-04 23:55:51 +0300

    added a function that takes only one argument

--- codeacademy/nemvarfun.py
+++ codeacademy/nemvarfun.py
@@ -5,3 +5,6 @@ print "arg1: %r, arg2: %r" % (arg1, arg2)
 # ok, that *args is actually pointless, we can just do this
 def print_two_again(arg1, arg2):
 print "arg1: %r, arg2: %r" % (arg1, arg2)
+# this just takes one argument
+def print_one(arg1):
+print "arg1: %r" % arg1
