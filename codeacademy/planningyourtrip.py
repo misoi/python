25a4045e4b4efd39c1ac97c68f6ff2f59c1195bd
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-05 13:47:56 +0300

    added a code for getting there,function called plane ride cost

--- codeacademy/planningyourtrip.py
+++ codeacademy/planningyourtrip.py
@@ -1,3 +1,15 @@
 def hotel_cost(nights):
     x=140*nights
     return x
+
+#getting there
+
+def plane_ride_cost(city):
+    if city=="Charlotte":
+        return 183
+    elif city=="Tampa":
+        return 220
+    elif city=="Pittsburgh":
+        return 222
+    else:
+        return 475
