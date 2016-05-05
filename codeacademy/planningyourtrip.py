fe0939814caf4b44aa04afff42fee4b10d3e4c05
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-05 13:50:06 +0300

    added a code for pulling them together

--- codeacademy/planningyourtrip.py
+++ codeacademy/planningyourtrip.py
@@ -25,3 +25,18 @@ def plane_ride_cost(city):
     else:
         total=(40*days)
         return total
+
+    # pulling them together
+
+    def rental_car_cost(days):
+    if days>=7:
+        total=(40*days)-50
+        return total
+    elif days>=3 and days<7:
+        total=(40*days)-20
+        return total
+    else:
+        total=(40*days)
+        return total
+def trip_cost(city,days):
+    return hotel_cost(days)+plane_ride_cost(city)+rental_car_cost(days)
