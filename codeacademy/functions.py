0714149a389bd4639c15e5481bf27c40f73c80a3
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-05 13:40:43 +0300

    added a function that calls another function

--- codeacademy/functions.py
+++ codeacademy/functions.py
@@ -7,3 +7,10 @@ def square(n):
 # Call the square function on line 9! Make sure to
 # include the number 10 between the parentheses.
 square(10)
+
+#function calling a function
+def one_good_turn(n):
+    return n + 1
+
+def deserves_another(n):
+    return one_good_turn(n)+ 2
