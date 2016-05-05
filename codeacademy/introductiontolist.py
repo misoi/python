a0b8d86d917e2042a20af7d1c684bd3c87ddca0e
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-05 13:55:23 +0300

    added appending list

--- codeacademy/introductiontolist.py
+++ codeacademy/introductiontolist.py
@@ -5,3 +5,18 @@ print numbers[0] + numbers[2]
 print "Adding the numbers at indices 1 and 3..."
 # Your code here!
 print numbers[1] + numbers[3]
+
+#appendind list
+suitcase = []
+suitcase.append("sunglasses")
+
+
+suitcase.append("toothpaste")# Your code here!
+suitcase.append("toothbrush")
+suitcase.append("jelly")
+
+
+list_length = len(suitcase) # Set this to the length of suitcase
+
+print "There are %d items in the suitcase." % (list_length)
+print suitcase
