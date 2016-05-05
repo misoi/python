f02827963cf58e60bb7213e3cb4890a76580ecf4
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-05 13:49:06 +0300

    added transportation part

--- codeacademy/planningyourtrip.py
+++ codeacademy/planningyourtrip.py
@@ -13,3 +13,15 @@ def plane_ride_cost(city):
         return 222
     else:
         return 475
+    # transportation part
+
+    def rental_car_cost(days):
+    if days>=7:
+        total=(40*days)-50
+        return total
+    elif days>=3 and days<7:
+        total=(40*days)-20
+        return total
+    else:
+        total=(40*days)
+        return total
