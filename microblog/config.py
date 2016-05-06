c7f854c18b6f8d3cb9ca70b1c59d81b5da763355
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-07 00:08:13 +0300

    added a config.py file to the root

---
+++ microblog/config.py
@@ -0,0 +1,2 @@
+WTF_CSRF_ENABLED=True # activates the cross-site request forgery prevention and it is enabled by default flask-WTF
+SECRET_KEY= 'YOU WILL NEVER GUESS' # only needed when CSRF is enabled , used to create cryptographic token that is used to validate forms
