1a297bd5f19c23f54be1c43983246d34de7d793c
misoi <cytnhia.chepkemoi@gmail.com>
2016-05-09 10:12:41 +0300

    created a base.html and inserted some words to index.html

--- practice/app/views.py
+++ practice/app/views.py
@@ -1,20 +1,16 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Cynthia'}
-    return '''
-<html>
-  <head>
-    <title>Home Page</title>
-  </head>
-  <body>
-    <h1>Hello, ''' + user['nickname'] + '''<br> Cynthia's blog</h1>
-
-  </body>
-</html>
-    '''
-
-
-
+    user = {'nickname': 'Cynthia'}  # fake user
+    posts = [  # fake array of post
+        {
+            'body': 'How to implement facebook API in your project'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
